507eda2c9cbc37e4744c074e5106714b7f8fe770
xbc <2196504-in____77v7@users.noreply.gitlab.com>
2021-04-06 23:24:34 +0900

    Add clickable text for privacy policy

--- app/src/main/java/com/example/tosmanager/ui/CreateAccountActivity.java
+++ app/src/main/java/com/example/tosmanager/ui/CreateAccountActivity.java
@@ -1,10 +1,23 @@
 package com.example.tosmanager.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.tosmanager.R;
@@ -14,7 +27,7 @@ import com.example.tosmanager.viewmodel.CreateAccountViewModel;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class CreateAccountActivity extends AppCompatActivity {
-    private static final String TAG = "CreateAccountActivity";
+    private static final String TAG = CreateAccountActivity.class.getName();
     private CreateAccountViewModel viewModel;
     // UI
     private Button createAccountButton;
@@ -23,6 +36,9 @@ public class CreateAccountActivity extends AppCompatActivity {
     TextInputEditText createAccountEmail;
     TextInputEditText createAccountPassword;
     TextInputEditText createAccountPasswordConfirm;
+
+    TextView createAccountNotice;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,5 +76,41 @@ public class CreateAccountActivity extends AppCompatActivity {
         viewModel.getIsCreatingAccount().observe(this, b -> {
             createAccountButton.setEnabled(!b);
         });
+
+        // 계정생성 주의사항
+        createAccountNotice = (TextView) findViewById(R.id.createAccountNotice);
+        createAccountNotice.setText(createNotice());
+        createAccountNotice.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    // 계정생성 주의사항 텍스트 생성
+    private CharSequence createNotice() {
+        String[] strings = getResources().getStringArray(R.array.text_privacy_policy_description);
+
+        SpannableStringBuilder builder = new SpannableStringBuilder();
+        for (int i = 0; i < strings.length; i++) {
+            SpannableString str = new SpannableString(strings[i]);
+
+            if (i == 1) {
+                // 링크 생성
+                str.setSpan(new ClickableSpan() {
+                    @Override
+                    public void onClick(@NonNull View widget) {
+
+                    }
+
+                    @Override
+                    public void updateDrawState(@NonNull TextPaint ds) {
+                        super.updateDrawState(ds);
+                        ds.setColor(getResources().getColor(R.color.design_default_color_primary));
+                        ds.bgColor = 0xffffffff;
+                        ds.setUnderlineText(false);
+                    }
+                }, 0, strings[i].length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            }
+
+            builder.append(str);
+        }
+        return builder;
     }
 }
