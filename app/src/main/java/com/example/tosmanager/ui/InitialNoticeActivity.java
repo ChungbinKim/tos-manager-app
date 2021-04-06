b445baaabaca9939082928d8089e33b3f32fc897
xbc <2196504-in____77v7@users.noreply.gitlab.com>
2021-04-07 00:25:12 +0900

    Start initial notice always when debugging

--- app/src/main/java/com/example/tosmanager/ui/InitialNoticeActivity.java
+++ app/src/main/java/com/example/tosmanager/ui/InitialNoticeActivity.java
@@ -1,27 +1,19 @@
 package com.example.tosmanager.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.Html;
-import android.text.SpannableString;
-import android.text.SpannableStringBuilder;
-import android.text.Spanned;
-import android.text.style.BackgroundColorSpan;
-import android.text.style.StyleSpan;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.tosmanager.BuildConfig;
 import com.example.tosmanager.R;
 import com.example.tosmanager.util.ForwardText;
-import com.example.tosmanager.viewmodel.CreateAccountViewModel;
 import com.example.tosmanager.viewmodel.InitialNoticeViewModel;
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -43,7 +35,7 @@ public class InitialNoticeActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         boolean isAgreed = sharedPreferences.getBoolean("is-agreed", false);
-        if (isAgreed) {
+        if (isAgreed && !BuildConfig.DEBUG) {
             proceed();
             return;
         }
