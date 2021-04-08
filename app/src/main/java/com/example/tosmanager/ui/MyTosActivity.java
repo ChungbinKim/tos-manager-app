d2e5509f15f7f2448705abd89bbf0709e28171eb
xbc <2196504-in____77v7@users.noreply.gitlab.com>
2021-04-08 20:58:57 +0900

    Set up fragments for bottom navigation

--- app/src/main/java/com/example/tosmanager/ui/MyTosActivity.java
+++ app/src/main/java/com/example/tosmanager/ui/MyTosActivity.java
@@ -1,8 +1,11 @@
 package com.example.tosmanager.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.tosmanager.R;
@@ -18,5 +21,34 @@ public class MyTosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_tos);
 
         BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
+
+        bottomNav.setSelectedItemId(R.id.navigationMyTos);
+        navigateTo(new MyTosFragment());
+    }
+
+    private boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment selectedFragment = null;
+        switch (item.getItemId()) {
+            case R.id.navigationMyTos:
+                selectedFragment = new MyTosFragment();
+                break;
+            case R.id.navigationCalendar:
+                selectedFragment = new CalendarFragment();
+                break;
+            case R.id.navigationConfiguration:
+                selectedFragment = new ConfigurationFragment();
+                break;
+        }
+
+        navigateTo(selectedFragment);
+        return true;
+    }
+
+    private void navigateTo(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frameLayout, fragment)
+                .commit();
     }
 }
