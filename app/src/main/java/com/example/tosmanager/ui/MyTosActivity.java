169a5a8ae581dff3544cc3d1165c4bbc056a5759
xbc <2196504-in____77v7@users.noreply.gitlab.com>
2021-04-08 20:11:10 +0900

    Add minimal bottom navigation bar

--- app/src/main/java/com/example/tosmanager/ui/MyTosActivity.java
+++ app/src/main/java/com/example/tosmanager/ui/MyTosActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 import com.example.tosmanager.R;
 import com.example.tosmanager.model.DataHolder;
 import com.example.tosmanager.model.LoginSession;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MyTosActivity extends AppCompatActivity {
 
@@ -16,13 +17,6 @@ public class MyTosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_tos);
 
-        // 아직은 로그인 결과만 출력
-        TextView t = (TextView) findViewById(R.id.tokenTest);
-        LoginSession session = DataHolder.getInstace().getLoginSession();
-        if (session != null) {
-            t.setText("Token: " + DataHolder.getInstace().getLoginSession().getToken());
-        } else {
-            t.setText("비로그인");
-        }
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
     }
 }
