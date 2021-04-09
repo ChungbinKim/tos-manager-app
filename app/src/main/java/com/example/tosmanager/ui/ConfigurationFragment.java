503c1d29f6ae4ef09e94a638c1764aa6dd6186c0
xbc <2196504-in____77v7@users.noreply.gitlab.com>
2021-04-09 23:45:24 +0900

    Display application version

--- app/src/main/java/com/example/tosmanager/ui/ConfigurationFragment.java
+++ app/src/main/java/com/example/tosmanager/ui/ConfigurationFragment.java
@@ -1,14 +1,19 @@
 package com.example.tosmanager.ui;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.tosmanager.BuildConfig;
 import com.example.tosmanager.R;
 
 public class ConfigurationFragment extends PreferenceFragmentCompat {
@@ -16,5 +21,15 @@ public class ConfigurationFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+        // 버전 설정
+        Preference versionPreference = findPreference("version");
+        String version;
+        try {
+            version = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            version = "Not Found";
+        }
+        versionPreference.setSummary(String.format("Version: %s", version));
     }
 }
