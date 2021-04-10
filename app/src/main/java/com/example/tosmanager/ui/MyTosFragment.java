95a1f644f865c1e73db3b3259417ec6c1da5adce
xbc <2196504-in____77v7@users.noreply.gitlab.com>
2021-04-10 20:00:30 +0900

    Add overall layout for MyTosFragment

--- app/src/main/java/com/example/tosmanager/ui/MyTosFragment.java
+++ app/src/main/java/com/example/tosmanager/ui/MyTosFragment.java
@@ -1,15 +1,22 @@
 package com.example.tosmanager.ui;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.tosmanager.R;
 
 public class MyTosFragment extends Fragment {
+    private static final String TAG = MyTosFragment.class.getName();
+
     public MyTosFragment() {
         // Required empty public constructor
     }
@@ -17,6 +24,8 @@ public class MyTosFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -24,5 +33,15 @@ public class MyTosFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_tos, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.my_tos, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
