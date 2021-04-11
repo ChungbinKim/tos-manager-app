10fe7c1d6a361d31c70eae892c57448bd7b2c012
xbc <2196504-in____77v7@users.noreply.gitlab.com>
2021-04-11 18:21:05 +0900

    Refactor password recover with RxAndroid

--- app/src/main/java/com/example/tosmanager/ui/RecoverPasswordActivity.java
+++ app/src/main/java/com/example/tosmanager/ui/RecoverPasswordActivity.java
@@ -12,6 +12,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
 import com.example.tosmanager.R;
+import com.example.tosmanager.util.CreateDialog;
 import com.example.tosmanager.util.ForwardText;
 import com.example.tosmanager.viewmodel.RecoverPasswordViewModel;
 import com.google.android.material.textfield.TextInputEditText;
@@ -23,8 +24,6 @@ public class RecoverPasswordActivity extends AppCompatActivity {
     // UI
     private Button recoverButton;
     TextInputEditText emailInput;
-
-    String Email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,38 +37,15 @@ public class RecoverPasswordActivity extends AppCompatActivity {
         // 비밀번호 찾기 버튼
         recoverButton = findViewById(R.id.recoverPasswordButton);
         recoverButton.setOnClickListener(v -> {
-            viewModel.recoverPassword(s -> {
-                Email = emailInput.getText().toString();
-                Response.Listener<String> responseListener = new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try
-                        {
-                            JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
-                            if(success){
-                                //회원정보 존재
-                                Toast.makeText(getApplicationContext(),"해당 email을 확인하세요", Toast.LENGTH_LONG).show();
-                            }
-                            else {
-                                //로그인 실패
-                                AlertDialog.Builder builder = new AlertDialog.Builder(RecoverPasswordActivity.this);
-                                builder.setMessage("일치하는 회원정보가 없습니다.")
-                                        .setNegativeButton("다시 시도",null)
-                                        .create()
-                                        .show();
-                            }
-
-                        } catch(Exception e)
-                        {
-                            e.printStackTrace();
-                        }
-                    }
-                };
-                RecoverRequest recoverRequest = new RecoverRequest(Email,responseListener);
-                RequestQueue queue = Volley.newRequestQueue(RecoverPasswordActivity.this);
-                queue.add(recoverRequest);
-            }, e -> {});
+            viewModel.recoverPassword(this).subscribe(s -> {
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+            }, e -> {
+                AlertDialog.Builder builder = new AlertDialog.Builder(RecoverPasswordActivity.this);
+                builder.setMessage(e.getMessage())
+                        .setNegativeButton("다시 시도",null)
+                        .create()
+                        .show();
+            });
         });
 
         // 이메일 입력
