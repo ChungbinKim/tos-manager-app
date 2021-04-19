502ae692a78d7b7931f7d946be073bced1497c78
xbc <2196504-in____77v7@users.noreply.gitlab.com>
2021-04-19 13:38:27 +0900

    Add dialog on ToS summary

--- app/src/main/java/com/example/tosmanager/ui/AddTosActivity.java
+++ app/src/main/java/com/example/tosmanager/ui/AddTosActivity.java
@@ -2,12 +2,15 @@ package com.example.tosmanager.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.tosmanager.R;
+import com.example.tosmanager.util.CreateDialog;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class AddTosActivity extends AppCompatActivity {
@@ -31,5 +34,13 @@ public class AddTosActivity extends AppCompatActivity {
                 input.setText(text);
             }
         }
+
+        Button summarizeButton = findViewById(R.id.addTosButton);
+        summarizeButton.setOnClickListener(v -> {
+            CreateDialog.createPrompt(this, R.string.text_summarize_dialog_message, (dialog, which) -> {
+                Intent i = new Intent(this, TosDetailsActivity.class);
+                startActivity(i);
+            }).show();
+        });
     }
 }
---
+++ app/src/main/java/com/example/tosmanager/ui/TosDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.tosmanager.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tosmanager.R;
+
+public class TosDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tos_details);
+    }
+}
