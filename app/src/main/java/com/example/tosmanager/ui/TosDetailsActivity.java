de37cd72a6675ca5038390ad2013ef00ea046154
xbc <2196504-in____77v7@users.noreply.gitlab.com>
2021-04-19 14:11:09 +0900

    Add menus for TosDetailsActivity

--- app/src/main/java/com/example/tosmanager/ui/TosDetailsActivity.java
+++ app/src/main/java/com/example/tosmanager/ui/TosDetailsActivity.java
@@ -1,17 +1,39 @@
 package com.example.tosmanager.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.tosmanager.R;
+import com.example.tosmanager.util.CreateDialog;
 
 public class TosDetailsActivity extends AppCompatActivity {
+    private boolean deleteConfirmed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tos_details);
         getSupportActionBar().setTitle(R.string.title_tos_details);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.tos_details, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        deleteConfirmed = false;
+        if (item.getItemId() == R.id.deleteTos) {
+            CreateDialog.createPrompt(this, R.string.delete_tos_dialog_message, (dialog, which) -> {
+                deleteConfirmed = true;
+            }).show();
+        }
+        return deleteConfirmed;
     }
 }
