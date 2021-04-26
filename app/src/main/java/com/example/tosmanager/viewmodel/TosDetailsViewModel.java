e3c961779edb8b6f84a1d27c66b9974672ab94fd
xbc <2196504-in____77v7@users.noreply.gitlab.com>
2021-04-26 17:55:49 +0900

    Construct mock summary

--- app/src/main/java/com/example/tosmanager/viewmodel/TosDetailsViewModel.java
+++ app/src/main/java/com/example/tosmanager/viewmodel/TosDetailsViewModel.java
@@ -3,6 +3,8 @@ package com.example.tosmanager.viewmodel;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.tosmanager.model.ListContents;
+import com.example.tosmanager.model.TableContent;
 import com.example.tosmanager.model.TermsSummary;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
@@ -18,7 +20,24 @@ public class TosDetailsViewModel extends ViewModel {
 
     public Single<TermsSummary> fetchTermsSummary(String id) {
         // TODO DB에서 가져온 후 TermsSummary로 변환
-        return Single.just(new TermsSummary(id, "휴면정책"))
+        TermsSummary dummySummary = new TermsSummary(id, "휴면정책");
+
+        ListContents lists = dummySummary.getListContents();
+
+        lists.addContent("사업자 권리")
+                .addItem("이 서비스는...")
+                .addItem("이 서비스는...")
+                .addItem("그리고 이 서비스는...");
+
+        lists.addContent("사업자 의무")
+                .addItem("이 서비스는...")
+                .addItem("그리고 이 서비스는");
+
+        dummySummary.getTableContent()
+                .addRow("휴면 기간", "9개월")
+                .addRow("장기 휴면시 계정 삭제", false);
+
+        return Single.just(dummySummary)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
