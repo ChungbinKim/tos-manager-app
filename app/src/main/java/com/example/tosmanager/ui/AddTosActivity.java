170c485e39c0d673dad6295b342174190955d5c9
xbc <2196504-in____77v7@users.noreply.gitlab.com>
2021-04-28 20:39:55 +0900

    Hide tutorial by configuration

--- app/src/main/java/com/example/tosmanager/ui/AddTosActivity.java
+++ app/src/main/java/com/example/tosmanager/ui/AddTosActivity.java
@@ -1,12 +1,16 @@
 package com.example.tosmanager.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Group;
+import androidx.preference.PreferenceManager;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.tosmanager.R;
@@ -15,6 +19,7 @@ import com.example.tosmanager.util.CreateDialog;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class AddTosActivity extends AppCompatActivity {
+    private LinearLayout tutorial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +37,11 @@ public class AddTosActivity extends AppCompatActivity {
             TextInputEditText input = findViewById(R.id.addTosInput);
             input.setText(inputText);
         }
+
+        // 아래 설명 부분
+        tutorial = findViewById(R.id.addTosTutorial);
+        int tutorialVisibility = PreferenceManager.getDefaultSharedPreferences(this).getBoolean("input_tutorial", true) ? View.VISIBLE : View.GONE;
+        tutorial.setVisibility(tutorialVisibility);
 
         Button summarizeButton = findViewById(R.id.addTosButton);
         summarizeButton.setOnClickListener(v -> {
