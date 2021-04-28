0a193e93b5a36291b2e692553fd681fc4103b9ac
xbc <2196504-in____77v7@users.noreply.gitlab.com>
2021-04-28 20:57:31 +0900

    Hide tutorial when do-not-show-again is checked

--- app/src/main/java/com/example/tosmanager/ui/AddTosActivity.java
+++ app/src/main/java/com/example/tosmanager/ui/AddTosActivity.java
@@ -6,10 +6,12 @@ import androidx.preference.PreferenceManager;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -19,13 +21,17 @@ import com.example.tosmanager.util.CreateDialog;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class AddTosActivity extends AppCompatActivity {
-    private LinearLayout tutorial;
+    private SharedPreferences sharedPreferences;
+    // UI
+    private CheckBox doNotShowAgain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_tos);
         getSupportActionBar().setTitle(R.string.title_input_tos);
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         // 설명 text
         TextView textInputByShare = findViewById(R.id.addTosInputByShare);
@@ -39,8 +45,8 @@ public class AddTosActivity extends AppCompatActivity {
         }
 
         // 아래 설명 부분
-        tutorial = findViewById(R.id.addTosTutorial);
-        int tutorialVisibility = PreferenceManager.getDefaultSharedPreferences(this).getBoolean("input_tutorial", true) ? View.VISIBLE : View.GONE;
+        LinearLayout tutorial = findViewById(R.id.addTosTutorial);
+        int tutorialVisibility = sharedPreferences.getBoolean("input_tutorial", true) ? View.VISIBLE : View.GONE;
         tutorial.setVisibility(tutorialVisibility);
 
         Button summarizeButton = findViewById(R.id.addTosButton);
@@ -50,5 +56,16 @@ public class AddTosActivity extends AppCompatActivity {
                 startActivity(i);
             }).show();
         });
+
+        doNotShowAgain = findViewById(R.id.addTosDoNotShowAgain);
+    }
+
+    @Override
+    protected void onDestroy() {
+        // 설명 다시 보지 않기 반영
+        if (doNotShowAgain.isChecked()) {
+            sharedPreferences.edit().putBoolean("input_tutorial", false).apply();
+        }
+        super.onDestroy();
     }
 }
